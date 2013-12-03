japandev07/Spring-FrameWork

commit b25e91a550beaf428a6e696959b717341a04f27d

Relax JavaBean rules for SpEL property access
Relax the method search algorithm used by `ReflectivePropertyAccessor`
to include methods of the form `getXY()` for properties of the form
`xy`.

Although the JavaBean specification indicates that a property `xy`
should use the accessors `getxY()` and `setxY()`, in practice many
developers choose to have an uppercase first character. The
`ReflectivePropertyAccessor` will now consider these style methods if
the traditional conventions fail to find a match.

Issue: SPR-10716

spring-expression/src/main/java/org/springframework/expression/spel/support/ReflectivePropertyAccessor.java
@@ -318,42 +318,34 @@ public class ReflectivePropertyAccessor implements PropertyAccessor {
 	 * Find a getter method for the specified property.
 	 */
 	protected Method findGetterForProperty(String propertyName, Class<?> clazz, boolean mustBeStatic) {
-		Method[] ms = getSortedClassMethods(clazz);
-		String propertyMethodSuffix = getPropertyMethodSuffix(propertyName);
-
-		// Try "get*" method...
-		String getterName = "get" + propertyMethodSuffix;
-		for (Method method : ms) {
-			if (method.getName().equals(getterName) && method.getParameterTypes().length == 0 &&
-					(!mustBeStatic || Modifier.isStatic(method.getModifiers()))) {
-				return method;
-			}
-		}
-		// Try "is*" method...
-		getterName = "is" + propertyMethodSuffix;
-		for (Method method : ms) {
-			if (method.getName().equals(getterName) && method.getParameterTypes().length == 0 &&
-					(boolean.class.equals(method.getReturnType()) || Boolean.class.equals(method.getReturnType())) &&
-					(!mustBeStatic || Modifier.isStatic(method.getModifiers()))) {
-				return method;
-			}
-		}
-		return null;
+		return findMethodForProperty(getPropertyMethodSuffixes(propertyName),
+				new String[] { "get", "is" }, clazz, mustBeStatic, 0);
 	}
 
 	/**
 	 * Find a setter method for the specified property.
 	 */
 	protected Method findSetterForProperty(String propertyName, Class<?> clazz, boolean mustBeStatic) {
+		return findMethodForProperty(getPropertyMethodSuffixes(propertyName),
+				new String[] { "set" }, clazz, mustBeStatic, 1);
+	}
+
+	private Method findMethodForProperty(String[] methodSuffixes, String[] prefixes, Class<?> clazz,
+			boolean mustBeStatic, int numberOfParams) {
 		Method[] methods = getSortedClassMethods(clazz);
-		String setterName = "set" + getPropertyMethodSuffix(propertyName);
-		for (Method method : methods) {
-			if (method.getName().equals(setterName) && method.getParameterTypes().length == 1 &&
-					(!mustBeStatic || Modifier.isStatic(method.getModifiers()))) {
-				return method;
+		for (String methodSuffix : methodSuffixes) {
+			for (String prefix : prefixes) {
+				for (Method method : methods) {
+					if (method.getName().equals(prefix + methodSuffix)
+							&& method.getParameterTypes().length == numberOfParams
+							&& (!mustBeStatic || Modifier.isStatic(method.getModifiers()))) {
+						return method;
+					}
+				}
 			}
 		}
 		return null;
+
 	}
 
 	/**
@@ -370,13 +362,29 @@ public class ReflectivePropertyAccessor implements PropertyAccessor {
 		return methods;
 	}
 
+	/**
+	 * Return the method suffixes for a given property name. The default implementation
+	 * uses JavaBean conventions with additional support for properties of the form 'xY'
+	 * where the method 'getXY()' is used in preference to the JavaBean convention of
+	 * 'getxY()'.
+	 */
+	protected String[] getPropertyMethodSuffixes(String propertyName) {
+		String suffix = getPropertyMethodSuffix(propertyName);
+		if (suffix.length() > 0 && Character.isUpperCase(suffix.charAt(0))) {
+			return new String[] { suffix };
+		}
+		return new String[] { suffix, StringUtils.capitalize(suffix) };
+	}
+
+	/**
+	 * Return the method suffix for a given property name. The default implementation
+	 * uses JavaBean conventions.
+	 */
 	protected String getPropertyMethodSuffix(String propertyName) {
 		if (propertyName.length() > 1 && Character.isUpperCase(propertyName.charAt(1))) {
 			return propertyName;
 		}
-		else {
-			return StringUtils.capitalize(propertyName);
-		}
+		return StringUtils.capitalize(propertyName);
 	}
 
 	/**

spring-expression/src/test/java/org/springframework/expression/spel/support/ReflectionHelperTests.java
@@ -335,6 +335,12 @@ public class ReflectionHelperTests extends ExpressionTestCase {
 		assertEquals("id",rpr.read(ctx,t,"Id").getValue());
 		assertTrue(rpr.canRead(ctx,t,"Id"));
 
+		// repro SPR-10994
+		assertEquals("xyZ",rpr.read(ctx,t,"xyZ").getValue());
+		assertTrue(rpr.canRead(ctx,t,"xyZ"));
+		assertEquals("xY",rpr.read(ctx,t,"xY").getValue());
+		assertTrue(rpr.canRead(ctx,t,"xY"));
+
 		// SPR-10122, ReflectivePropertyAccessor JavaBean property names compliance tests - setters
 		rpr.write(ctx, t, "pEBS","Test String");
 		assertEquals("Test String",rpr.read(ctx,t,"pEBS").getValue());
@@ -429,6 +435,8 @@ public class ReflectionHelperTests extends ExpressionTestCase {
 		String id = "id";
 		String ID = "ID";
 		String pEBS = "pEBS";
+		String xY = "xY";
+		String xyZ = "xyZ";
 
 		public String getProperty() { return property; }
 		public void setProperty(String value) { property = value; }
@@ -444,6 +452,10 @@ public class ReflectionHelperTests extends ExpressionTestCase {
 		public String getId() { return id; }
 
 		public String getID() { return ID; }
+
+		public String getXY() { return xY; }
+
+		public String getXyZ() { return xyZ; }
 
 		public String getpEBS() {
 			return pEBS;

commit 1ebc2acc84f70c8d8af97203134462fd286c8dcc

Only consider "is" methods with boolean returns
Fix regression introduced in b25e91a5 where ReflectivePropertyAccessor
does not consider the return type for "is" getters.

Issue: SPR-11142

spring-expression/src/main/java/org/springframework/expression/spel/support/ReflectivePropertyAccessor.java
@@ -22,8 +22,11 @@ import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.core.MethodParameter;
@@ -49,6 +52,17 @@ import org.springframework.util.StringUtils;
  * @since 3.0
  */
 public class ReflectivePropertyAccessor implements PropertyAccessor {
+
+	private static final Set<Class<?>> BOOLEAN_TYPES;
+	static {
+		Set<Class<?>> booleanTypes = new HashSet<Class<?>>();
+		booleanTypes.add(Boolean.class);
+		booleanTypes.add(Boolean.TYPE);
+		BOOLEAN_TYPES = Collections.unmodifiableSet(booleanTypes);
+	}
+
+	private static final Set<Class<?>> ANY_TYPES = Collections.emptySet();
+
 
 	private final Map<CacheKey, InvokerPair> readerCache = new ConcurrentHashMap<CacheKey, InvokerPair>(64);
 
@@ -319,8 +333,13 @@ public class ReflectivePropertyAccessor implements PropertyAccessor {
 	 * Find a getter method for the specified property.
 	 */
 	protected Method findGetterForProperty(String propertyName, Class<?> clazz, boolean mustBeStatic) {
-		return findMethodForProperty(getPropertyMethodSuffixes(propertyName),
-				new String[] { "get", "is" }, clazz, mustBeStatic, 0);
+		Method method = findMethodForProperty(getPropertyMethodSuffixes(propertyName),
+				 "get", clazz, mustBeStatic, 0, ANY_TYPES);
+		if (method == null) {
+			method = findMethodForProperty(getPropertyMethodSuffixes(propertyName),
+					 "is", clazz, mustBeStatic, 0, BOOLEAN_TYPES);
+		}
+		return method;
 	}
 
 	/**
@@ -328,20 +347,19 @@ public class ReflectivePropertyAccessor implements PropertyAccessor {
 	 */
 	protected Method findSetterForProperty(String propertyName, Class<?> clazz, boolean mustBeStatic) {
 		return findMethodForProperty(getPropertyMethodSuffixes(propertyName),
-				new String[] { "set" }, clazz, mustBeStatic, 1);
+				"set", clazz, mustBeStatic, 1, ANY_TYPES);
 	}
 
-	private Method findMethodForProperty(String[] methodSuffixes, String[] prefixes, Class<?> clazz,
-			boolean mustBeStatic, int numberOfParams) {
+	private Method findMethodForProperty(String[] methodSuffixes, String prefix, Class<?> clazz,
+			boolean mustBeStatic, int numberOfParams, Set<Class<?>> requiredReturnTypes) {
 		Method[] methods = getSortedClassMethods(clazz);
 		for (String methodSuffix : methodSuffixes) {
-			for (String prefix : prefixes) {
-				for (Method method : methods) {
-					if (method.getName().equals(prefix + methodSuffix)
-							&& method.getParameterTypes().length == numberOfParams
-							&& (!mustBeStatic || Modifier.isStatic(method.getModifiers()))) {
-						return method;
-					}
+			for (Method method : methods) {
+				if (method.getName().equals(prefix + methodSuffix)
+						&& method.getParameterTypes().length == numberOfParams
+						&& (!mustBeStatic || Modifier.isStatic(method.getModifiers()))
+						&& (requiredReturnTypes.isEmpty() || requiredReturnTypes.contains(method.getReturnType()))) {
+					return method;
 				}
 			}
 		}

spring-expression/src/test/java/org/springframework/expression/spel/SpelReproTests.java
@@ -1852,6 +1852,17 @@ public class SpelReproTests extends ExpressionTestCase {
 		assertTrue(classNameExpression.getValue(map,Boolean.class));
 	}
 
+	@Test
+	public void SPR_11142() throws Exception {
+		SpelExpressionParser parser = new SpelExpressionParser();
+		StandardEvaluationContext context = new StandardEvaluationContext();
+		SPR11142 rootObject = new SPR11142();
+		Expression expression = parser.parseExpression("something");
+		thrown.expect(SpelEvaluationException.class);
+		thrown.expectMessage("property 'something' cannot be found");
+		expression.getValue(context, rootObject);
+	}
+
 
 	private static enum ABC {A, B, C}
 
@@ -1953,6 +1964,14 @@ public class SpelReproTests extends ExpressionTestCase {
 				return string.equals(((TestClass2)o).string);
 			}
 			return false;
+		}
+
+	}
+
+	static class SPR11142 {
+
+		public String isSomething() {
+			return "";
 		}
 
 	}