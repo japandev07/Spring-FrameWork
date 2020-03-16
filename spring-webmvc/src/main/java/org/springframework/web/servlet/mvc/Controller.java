japandev07/Spring-FrameWork

commit 7b6d83a106a0e2267f40d57d2aa24503196533af

Use Gradle test fixture support for spring-web
See gh-23550

spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/Controller.java
@@ -100,8 +100,8 @@ import org.springframework.web.servlet.ModelAndView;
  * @see LastModified
  * @see SimpleControllerHandlerAdapter
  * @see AbstractController
- * @see org.springframework.mock.web.MockHttpServletRequest
- * @see org.springframework.mock.web.MockHttpServletResponse
+ * @see org.springframework.web.testfixture.servlet.MockHttpServletRequest
+ * @see org.springframework.web.testfixture.servlet.MockHttpServletResponse
  * @see org.springframework.context.ApplicationContextAware
  * @see org.springframework.context.ResourceLoaderAware
  * @see org.springframework.web.context.ServletContextAware

commit 2f5fa1192f17bf8ca3e2322c5f30319e68f9cd78

Fix Javadoc links accidentally broken in 7b6d83a106

spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/Controller.java
@@ -100,8 +100,8 @@ import org.springframework.web.servlet.ModelAndView;
  * @see LastModified
  * @see SimpleControllerHandlerAdapter
  * @see AbstractController
- * @see org.springframework.web.testfixture.servlet.MockHttpServletRequest
- * @see org.springframework.web.testfixture.servlet.MockHttpServletResponse
+ * @see org.springframework.mock.web.MockHttpServletRequest
+ * @see org.springframework.mock.web.MockHttpServletResponse
  * @see org.springframework.context.ApplicationContextAware
  * @see org.springframework.context.ResourceLoaderAware
  * @see org.springframework.web.context.ServletContextAware